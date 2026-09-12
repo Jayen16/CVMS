3894064aefa7fd42a2826d12a62a772624fae94f
Jayen <ninaklarisse.ulangkaya@cvsu.edu.ph>
2026-09-13 01:44:54 +0800

    display image logo and text logo as backup

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -5,13 +5,13 @@
 @if($sidebar)
     <flux:sidebar.brand name="{{ config('rhu.short_name') }}" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-10 items-center justify-center">
-            <span class="rhu-monogram size-10 text-[0.6rem]" aria-hidden="true">RHU</span>
+            <x-app-logo-icon class="size-10" />
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:brand name="{{ config('rhu.short_name') }}" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-10 items-center justify-center">
-            <span class="rhu-monogram size-10 text-[0.6rem]" aria-hidden="true">RHU</span>
+            <x-app-logo-icon class="size-10" />
         </x-slot>
     </flux:brand>
 @endif
